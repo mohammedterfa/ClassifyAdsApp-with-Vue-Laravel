9316a223e80eaec7d1ab85e2a007daf484d91c8a
mohammedterfa <mohammedterfa@gmail.com>
2022-11-05 10:40:58 +0200

    register

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="text-center mt-2"><h4>Register</h4></div>
+                    <div class="card-body"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
