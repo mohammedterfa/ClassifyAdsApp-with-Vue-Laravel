3a478539e123c30dcc6b65db678389808b00cd51
mohammedterfa <mohammedterfa@gmail.com>
2022-11-23 12:39:07 +0200

    code added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="slider/slider1.png" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="slider/slider2.png" class="d-block w-100" alt="...">
-      </div>
-      <div class="carousel-item">
-        <img src="slider/slider3.png" class="d-block w-100" alt="...">
-      </div>
+<div class="slider" style="margin-top: -25px;">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="slider/slider1.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="slider/slider2.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="slider/slider3.png" class="d-block w-100" alt="...">
+        </div>
+        </div>
     </div>
-  </div>
+</div>
 @endsection
