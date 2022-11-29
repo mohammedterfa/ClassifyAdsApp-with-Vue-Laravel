642313df17a71406ce4f1c7490116668af4ecc7d
mohammedterfa <mohammedterfa@gmail.com>
2022-11-29 14:07:01 +0200

    code added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,14 +19,67 @@
 <div class="container mt-5">
     <span>
         <h1>Car</h1>
+        <a href="#" class="float-end">View all</a>
     </span>
     <div id="carouselExampleFade" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="..." class="d-block w-100" alt="...">
+            <div class="row">
+                <div class="col-3">
+                    <img src="product/car1.jpg" class="img-thumbnail" alt="...">
+                    <p class="text-center card-footer" style="color:#222">Name of product/$5000</p>
+
+                </div>
+
+
+                <div class="col-3">
+                    <img src="product/car1.jpg" class="img-thumbnail" alt="...">
+                    <p class="text-center card-footer" style="color:#222">Name of product/$5000</p>
+
+                </div>
+
+
+                <div class="col-3">
+                    <img src="product/car1.jpg" class="img-thumbnail" alt="...">
+                    <p class="text-center card-footer" style="color:#222">Name of product/$5000</p>
+
+                </div>
+
+
+                <div class="col-3">
+                    <img src="product/car1.jpg" class="img-thumbnail" alt="...">
+                    <p class="text-center card-footer" style="color:#222">Name of product/$5000</p>
+                </div>
+            </div>
         </div>
         <div class="carousel-item">
-            <img src="product/car1.jpg" class="d-block w-100" alt="...">
+            <div class="row">
+                <div class="col-3">
+                    <img src="product/car2.jpg" class="img-thumbnail" alt="...">
+                    <p class="text-center card-footer" style="color:#222">Name of product/$5000</p>
+
+                </div>
+
+
+                <div class="col-3">
+                    <img src="product/car2.jpg" class="img-thumbnail" alt="...">
+                    <p class="text-center card-footer" style="color:#222">Name of product/$5000</p>
+
+                </div>
+
+
+                <div class="col-3">
+                    <img src="product/car2.jpg" class="img-thumbnail" alt="...">
+                    <p class="text-center card-footer" style="color:#222">Name of product/$5000</p>
+
+                </div>
+
+
+                <div class="col-3">
+                    <img src="product/car2.jpg" class="img-thumbnail" alt="...">
+                    <p class="text-center card-footer" style="color:#222">Name of product/$5000</p>
+                </div>
+            </div>
         </div>
 
         </div>
