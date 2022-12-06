494e9f020ad390b5a0abc9a566dd005a76512ae3
mohammedterfa <mohammedterfa@gmail.com>
2022-12-06 19:03:03 +0200

    injected files in index admin dashboard file

--- resources/views/backend/admin/index.blade.php
+++ resources/views/backend/admin/index.blade.php
@@ -1,5 +1,16 @@
-@include('backend.layouts.header')
-@include('backend.layouts.sidebar')
-@include('backend.layouts.content')
-@include('backend.layouts.footer')
+@extends('backend.layouts.master')
+@section('content')
+<div class="main-panel">
+    <div class="content-wrapper">
+      <div class="row">
+        <div class="col-md-12 grid-margin">
+            <div class="d-flex justify-content-between flex-wrap">
+                body
+            </div>
+        </div>
+      </div>
+    </div>
+</div>
+</div>
 
+@endsection
--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -1,4 +1,5 @@
 <!-- partial:partials/_footer.html -->
+</div>
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
       <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
