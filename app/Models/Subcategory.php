c9ae49a8d67e9d6f89fdefaddecd27e4dc1e559b
mohammedterfa <mohammedterfa@gmail.com>
2022-12-25 13:34:14 +0200

    make fillable subcategory

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Subcategory extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'name',
+        'category_id',
+        'slug'
+    ];
 }
